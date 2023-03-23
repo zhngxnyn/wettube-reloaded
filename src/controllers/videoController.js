24c0807f2597a16410e5b667cd36e48b5c801583
현원 <jhwjhwjhw97@gmail.com>
2023-03-23 20:47:24 +0900

    update

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,6 +1,43 @@
-export const trending = (req, res) => res.render("home", { pageTitle: "Home" });
+const fakeUser = {
+  username: "Nicolas",
+  loggedIn: true,
+};
+// const videos = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const videos = [
+  {
+    title: "First Video",
+    rating: 5,
+    comments: 2,
+    createdAt: "2 minutes ago",
+    views: 59,
+    id: 1,
+  },
+  {
+    title: "Second Video",
+    rating: 5,
+    comments: 2,
+    createdAt: "2 minutes ago",
+    views: 59,
+    id: 2,
+  },
+  {
+    title: "Third Video",
+    rating: 5,
+    comments: 2,
+    createdAt: "2 minutes ago",
+    views: 59,
+    id: 3,
+  },
+];
+export const trending = (req, res) => {
+  return res.render("home", { pageTitle: "Home", videos });
+};
+
 export const see = (req, res) => {
-  return res.render("Watch");
+  const { id } = req.params;
+  const video = videos[id - 1];
+  console.log("show video", id);
+  return res.render("watch", { pageTitle: `Watching ${video.title}` });
 };
 export const edit = (req, res) => res.send("Edit");
 export const search = (req, res) => res.send("Search");
